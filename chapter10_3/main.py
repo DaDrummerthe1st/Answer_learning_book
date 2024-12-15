1b8c6ba1260378db7b610a377a18740ecf079b1b
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2024-12-15 23:02:30 +0100

    Added answers and notes to chapter 10 plus a TODO

---
+++ chapter10_3/main.py
@@ -0,0 +1,5 @@
+import two_functions
+
+# TODO How is DOCSTRING_TEST answered with an exception?
+
+print(two_functions.add_two_numbers(4, 6))
---
+++ chapter10_3/two_functions.py
@@ -0,0 +1,27 @@
+def add_two_numbers(x: int, y=5: int) --> None:
+    """Module is about adding the numbers
+    >>> add_two_numbers(3)
+    8
+    >>> add_two_numbers(5, 2)
+    7
+    >>> add_two_numbers("torsdag")
+    exception
+    
+    """
+    try:
+        if x == int or y == int:
+            return x + y
+    except Exception as e:
+        print("Unknown error: " + str(e))
+    except:
+        print("Even more unknown error!!!!")
+
+def subtract_two_numbers(x, y=5):
+    try:
+        if x == int or y == int:
+            return x - y
+    except Exception as e:
+        print("Unknown error: " + str(e))
+    except:
+        print("Even more unknown error!!!!")
+
